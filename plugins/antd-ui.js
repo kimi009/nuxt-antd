3ba13db79b8e907bcf92b3c868fdc0ad0f62edf4
kimi009 <444812002@qq.com>
2020-03-23 21:09:09 +0800

    test

---
+++ plugins/antd-ui.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+
+import Row from 'ant-design-vue/lib/row'
+import Col from 'ant-design-vue/lib/col'
+import Button from 'ant-design-vue/lib/button'
+import Menu from 'ant-design-vue/lib/menu'
+import Icon from 'ant-design-vue/lib/icon'
+import Table from 'ant-design-vue/lib/table'
+import Tag from 'ant-design-vue/lib/tag'
+import Divide from 'ant-design-vue/lib/divider'
+import Card from 'ant-design-vue/lib/card'
+
+import 'ant-design-vue/dist/antd.css'
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Button)
+Vue.use(Menu)
+Vue.use(Icon)
+Vue.use(Table)
+Vue.use(Tag)
+Vue.use(Divide)
+Vue.use(Card)
